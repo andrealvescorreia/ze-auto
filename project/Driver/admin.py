3eb53d3cef8cbe1b5e4c7a1f5d1215041d3cd7c1
Weslley Almeida <weslley.almeida.f@gmail.com>
2023-04-03 15:30:37 -0300

    AntesSerializers

--- project/Driver/admin.py
+++ project/Driver/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Mecanico, Veiculo
+from .models import Mecanico, Veiculo, Avaliacao, Manutencao
 
-admin.site.register((Veiculo, Mecanico))
+admin.site.register((Veiculo, Mecanico, Avaliacao, Manutencao))
 # Register your models here.
