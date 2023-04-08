16d53e58514aaa0ca06a94af76eb27f4943a0536
André Alves Correia <andre2013aguabranca@gmail.com>
2023-04-08 09:00:05 -0300

    Footer

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import './style.scss'
+
+const Footer = () => {
+  return <footer id="home-footer">
+    <p id="footer-info">UEPB - Engenharia de Software II</p>
+    <p id="footer-copyright">© 2023-Zé auto</p>
+  </footer>;
+}
+ 
+export default Footer;
